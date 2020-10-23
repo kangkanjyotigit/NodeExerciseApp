23ad9004385718fa2c0e9d94c246d01d43c72b65
Kangkan Jyoti Barman <kangkan.400@gmail.com>
2020-10-24 02:51:12 +0530

    Adding the changes

--- server.js
+++ server.js
@@ -8,11 +8,61 @@ const cors = require('cors');
 const url="mongodb+srv://kangkan:Pathsala@123@mygrocery-u9tcl.mongodb.net/test?retryWrites=true&w=majority";
 
 const dbName = "WorkShop";
-
-// router.get('/',async(req,res)=>{
-//     res.send('The App is working');
-// })
  
+router.post('/register', cors(), async (req, res, next) => {
+    try {
+        MongoClient.connect(url, function (err, client) {
+            assert.strictEqual(null, err);
+            const db = client.db(dbName);
+            let userdata = req.body;
+            const collection = db.collection('UserDetails');
+            collection.insertOne(userdata, (err, results) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    let payload = { subject: results._id };
+                    let token = jwt.sign(payload, 'secretkey')
+                    res.status(200).send({ token, id: results._id });
+                }
+            })
+            client.close();
+        });
+    }
+    catch (err) {
+        next(err);
+    }
+});
+router.post('/login', cors(), async (req, res, next) => {
+    try {
+        MongoClient.connect(url, function (err, client) {
+            assert.strictEqual(null, err);
+            const db = client.db(dbName);
+            let userdata = req.body;
+            const collection = db.collection('UserDetails');
+            collection.findOne({ email: userdata.email }, (err, results) => {
+                if (err) {
+                    console.log('Printing the error' + err);
+                } else
+                    if (!results) {
+                        res.status(401).send('user not found');
+                    } else
+                        if (userdata.password != results.password) {
+                            res.status(401).send('password doesnt match');
+                        } else {
+                            let payload = { subject: results._id };
+                            console.log('printing the id' + results._id);
+                            let token = jwt.sign(payload, 'secretkey');
+                            res.status(200).send({ token, id: results._id });
+                        }
+            })
+            client.close();
+        });
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
 router.get('/',cors(),async(req,res,next)=>{
     try {
         MongoClient.connect(url, function (err, client) {
